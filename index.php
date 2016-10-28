2eaec841e098115f13f752d5a5bbace68f6ea762
wsslzwrs <laterna@imac-van-wessel.home>
2016-10-28 18:28:55 +0200

    mapstructuur

--- index.php
+++ index.php
@@ -1,9 +1,56 @@
 <html>
 <head>
 
+<title>Start vandaag nog met AppCare</title>
+
+<!-- meta description -->
+
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+
 </head>
 <body>
 
+<div id="container-page">
+	<div id="zone-header">
+		<h1>Start vandaag nog met AppCare</h1>
+		<p>
+			Een webcaretool maakt het gemakkelijk.
+		</p>
+	</div>
+	<div id="zone-usp">
+		<div class="one-usp">
+			<img src="assets/whatsapp.png" width="241" height="241" border="0" />
+			<h3>Geintegreerde oplossing</h3>
+			<p>
+				Integreer WhatsApp met een webcaretool voor een optimale klantbeleving.
+			</p>
+		</div>
+		<div class="one-usp">
+			<img src="assets/messages.png" width="241" height="241" border="0" />
+			<h3>Betere service</h3>
+			<p>
+				Berichten worden automatisch aan de juiste contactpersoon of afdeling toegewezen en overzichtelijk gesorteerd.
+			</p>
+		</div>
+		<div class="one-usp">
+			<img src="assets/kpis.png" width="241" height="241" border="0" />
+			<h3>Meet je KPI's</h3>
+			<p>
+				Meet dagelijks hoeveel klanten je daadwerkelijk helpt door gebruik te maken van de unieke client case structuur.
+			</p>
+		</div>
+	</div>
+	<div id="zone-contact">
+		<h2>Meer weten over webcare via WhatsApp?</h2>
+		<p>
+			Laat dan hier je gegevens achter en we namen z.s.m. contact met je op!
+		</p>
+		<form>
+			.....
+		</form>
+	</div>
+</div>
 
 </body>
+</html>
 
