f486fde5b21f844418b380e587688ec0d9e6123a
wsslzwrs <laterna@imac-van-wessel.home>
2016-10-29 11:32:12 +0200

    styling

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 
 <!-- meta description -->
 
+<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=PT+Sans">
 <link rel="stylesheet" type="text/css" href="css/coosto.css">
 
 </head>
@@ -12,42 +13,48 @@
 
 <div id="container-page">
 	<div id="zone-header">
-		<h1>Start vandaag nog met AppCare</h1>
-		<p>
-			Een webcaretool maakt het gemakkelijk.
-		</p>
+		<div id="zone-header-wrap">
+			<h1>Start vandaag nog met AppCare</h1>
+			<p>
+				Een webcaretool maakt het gemakkelijk
+			</p>
+		</div>
 	</div>
 	<div id="zone-usp">
-		<div class="one-usp">
-			<img src="assets/whatsapp.png" width="241" height="241" border="0" />
-			<h3>Geintegreerde oplossing</h3>
-			<p>
-				Integreer WhatsApp met een webcaretool voor een optimale klantbeleving.
-			</p>
-		</div>
-		<div class="one-usp">
-			<img src="assets/messages.png" width="241" height="241" border="0" />
-			<h3>Betere service</h3>
-			<p>
-				Berichten worden automatisch aan de juiste contactpersoon of afdeling toegewezen en overzichtelijk gesorteerd.
-			</p>
-		</div>
-		<div class="one-usp">
-			<img src="assets/kpis.png" width="241" height="241" border="0" />
-			<h3>Meet je KPI's</h3>
-			<p>
-				Meet dagelijks hoeveel klanten je daadwerkelijk helpt door gebruik te maken van de unieke client case structuur.
-			</p>
+		<div id="zone-usp-wrap">
+			<div class="one-usp">
+				<img src="assets/whatsapp.png" />
+				<h2>Geïntegreerde oplossing</h2>
+				<p>
+					Integreer WhatsApp met een webcaretool voor een optimale klantbeleving.
+				</p>
+			</div>
+			<div class="one-usp">
+				<img src="assets/messages.png" />
+				<h2>Betere service</h2>
+				<p>
+					Berichten worden automatisch aan de juiste contactpersoon of afdeling toegewezen en overzichtelijk gesorteerd.
+				</p>
+			</div>
+			<div class="one-usp">
+				<img src="assets/kpis.png" />
+				<h2>Meet je KPI's</h2>
+				<p>
+					Meet dagelijks hoeveel klanten je daadwerkelijk helpt door gebruik te maken van de unieke client case structuur.
+				</p>
+			</div>
 		</div>
 	</div>
 	<div id="zone-contact">
-		<h2>Meer weten over webcare via WhatsApp?</h2>
-		<p>
-			Laat dan hier je gegevens achter en we namen z.s.m. contact met je op!
-		</p>
-		<form>
-			.....
-		</form>
+		<div id="zone-contact-wrap">
+			<h3>Meer weten over webcare via WhatsApp?</h3>
+			<p>
+				Laat dan hier je gegevens achter en we nemen z.s.m. contact met je op!
+			</p>
+			<form>
+				.....
+			</form>
+		</div>
 	</div>
 </div>
 
