5d3043c5c36f108c2d1e8558bc138e1d8eb9dae8
wsslzwrs <laterna@192.168.2.126>
2016-10-30 09:28:24 +0100

    formulier: markup + validatie

--- index.php
+++ index.php
@@ -51,9 +51,37 @@
 			<p>
 				Laat dan hier je gegevens achter en we nemen z.s.m. contact met je op!
 			</p>
-			<form>
-				.....
+			<form method="post" name="coosto" id="zone-contact-form" action="https://api.formbucket.com/f/buk_Vhno2F2Te8YnW2TzfCkVBGqH">
+				<div class="form-row">
+					<div class="form-row-left">
+						<input type="text" name="first_name" placeholder="Voornaam" required />
+					</div>
+					<div class="form-row-right">
+						<input type="text" name="last_name" placeholder="Achternaam" required />
+					</div>
+				</div>
+				<div class="form-row">
+					<input type="text" name="company" placeholder="Bedrijf" required />
+				</div>
+				<div class="form-row">
+					<div class="form-row-left">
+						<input type="email" name="email" placeholder="Zakelijk e-mail" required />
+					</div>
+					<div class="form-row-right">
+						<input type="phone" name="phone" placeholder="Telefoonnummer" required />
+					</div>
+				</div>
+				<div class="form-row">
+					<textarea id="comments" placeholder="Bericht (optioneel)" rows="5" cols="50"></textarea>
+				</div>
+				<div class="form-row">
+					<button type="submit">Plan een gratis demonstratie</button>
+				</div>
+				<input type="hidden" name="time" value="<?php echo date("G:i"); ?>" />
 			</form>
+			<p class="footnote">
+				Met het versturen van dit formulier sta je toe dat wij je gerelateerde e-mails sturen.
+			</p>
 		</div>
 	</div>
 </div>
